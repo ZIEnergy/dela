50e480f33c951a2ccf00575859310ac4752be6cd
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-01 21:58:26 +0300

    add map balloons

--- build/js/script.js
+++ build/js/script.js
@@ -47,4 +47,4 @@ var mapContainer=document.getElementById("map"),myOptions={center:new google.map
 
 
 
-$(".main").fullpage({verticalCentered:!1,animateAnchor:!0,navigation:!0,navigationTooltips:["Первый шаг","Как работает","Преимущества","Что заказывают?","Категории","Узнать больше"],onLeave:function(n,a,e){1==n&&"down"==e&&$("#fp-nav").fadeIn("slow"),1==a&&$("#fp-nav").hide()}});})
+$(".main").fullpage({verticalCentered:!1,animateAnchor:!0,navigation:!0,navigationTooltips:["Первый шаг","Как работает","Преимущества","Что заказывают?","Категории","Узнать больше"],onLeave:function(a,n,e){1==a&&"down"==e&&$("#fp-nav").fadeIn("slow"),1==n&&$("#fp-nav").hide(),3==n&&$("#fp-nav").addClass("fp-nav--white"),3==a&&$("#fp-nav").removeClass("fp-nav--white")}});})
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,5 +10,11 @@ $('.main').fullpage({
     if(nextIndex == 1){
       $('#fp-nav').hide();
     }
+    if(nextIndex == 3){
+      $('#fp-nav').addClass('fp-nav--white');
+    }
+    if(index == 3){
+      $('#fp-nav').removeClass('fp-nav--white');
+    }
   }
 });
