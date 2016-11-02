330ebd5f608fe1f8c5989b54f806b6cbfc1f1cff
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-02 16:09:21 +0300

    add offer-popup

--- build/js/script.js
+++ build/js/script.js
@@ -50,4 +50,5 @@ $(".modal-link").magnificPopup({type:"inline"});
 
 
 
+
 $(".main").fullpage({verticalCentered:!1,animateAnchor:!0,navigation:!0,navigationTooltips:["Первый шаг","Как работает","Преимущества","Что заказывают?","Категории","Узнать больше"],onLeave:function(a,n,e){1==a&&"down"==e&&$("#fp-nav").fadeIn("slow"),1==n&&$("#fp-nav").hide(),3==n&&$("#fp-nav").addClass("fp-nav--white"),3==a&&$("#fp-nav").removeClass("fp-nav--white")}});})
